d8af4017c9d0348ec8a867554e7309cfcdb08ed9
Maksim Fesenko <drresist@gmail.com>
2022-08-17 08:16:09 +0300

    add new tasks from codebascis

--- CodeBasics/clojure/src/core.clj
+++ CodeBasics/clojure/src/core.clj
@@ -35,13 +35,33 @@
     (<= level 20) "middle"
     (> level 20) "senior"))
 
+; Case + cond + if combo
+
 (defn do-today
-  [weekday])
+  [weekday]
+  (if (int? weekday)
+    (cond
+      (and (>= weekday 1) (<= weekday 5)) "work"
+      (case weekday
+        (6 7) true
+        false) "rest"
+      :else "???")
+      "???"))
 
 (defn -main []
   (println "(-main invoked)"))
 
+
+
 (comment
+  (do-today 1)
+  (do-today 0)
+  (do-today -1)
+  (do-today 10)
+  (do-today false)
+  (do-today "test")
+  (do-today 6)
+
   (leap-year? 2012) ; true
   (leap-year? 1913) ; false
   (leap-year? 1804) ; true
